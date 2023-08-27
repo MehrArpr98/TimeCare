2116212a10f0512a5369a963c9fd959ed1b2a428
mehrnoosh <mehr@gmail.com>
2023-08-27 19:50:14 +0330

    add default homepage

--- src/components/Introducing.vue
+++ src/components/Introducing.vue
@@ -3,13 +3,13 @@
   <div class="grid grid-cols-1 lg:grid-cols-2 gap-0 relative">
     <div id="box"></div>
     <div class="col-span-1 flex items-center justify-center">
-      <img src="assets/home.jpg" />
+      <img src="../../public/assets/home.jpg" />
     </div>
 
     <div class="col-span-1 relative">
       
-      <img src="assets/shape1.png" class="h-60 absolute right-0 -z-10" />
-      <img src="assets/shape2.png" class="w-40 absolute left-0 top-1/4 -z-10" />
+      <img src="../../public/assets/shape1.png" class="h-60 absolute right-0 -z-10" />
+      <img src="../../public/assets/shape2.png" class="w-40 absolute left-0 top-1/4 -z-10" />
 
       <div class="pb-6 text-5xl text-center font-bold leading-snug">
         Stay organized and efficient with <span class="animate-bounce ">TimeCare</span>
